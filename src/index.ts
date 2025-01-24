ecad6c3d8eb1a190c2f179539de27791238a93fc
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-01-24 22:20:39 +0530

    deployment fixes

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,45 @@ import ValidateUser from './UserValidation';
 import fs from 'fs';
 import https from 'https';
 dotenv.config();
+
 const PORT = process.env.PORT || 7079;
 const SSL_KEY_PATH = process.env.SSL_KEY_PATH || '/etc/certs/privkey.pem';
 const SSL_CERT_PATH = process.env.SSL_CERT_PATH || '/etc/certs/fullchain.pem';
 
-
-const serverOptions = {
-  key: fs.readFileSync(SSL_KEY_PATH),
-  cert: fs.readFileSync(SSL_CERT_PATH),
+let serverOptions: { key: string | Buffer; cert: string | Buffer } = {
+  key: '',
+  cert: '',
 };
 
-const httpsServer = https.createServer(serverOptions);
+try {
+  if (fs.existsSync(SSL_KEY_PATH) && fs.existsSync(SSL_CERT_PATH)) {
+    serverOptions.key = fs.readFileSync(SSL_KEY_PATH);
+    serverOptions.cert = fs.readFileSync(SSL_CERT_PATH);
+  } else {
+    console.error(
+      'SSL certificate files are missing. Continuing with HTTP server.'
+    );
+    serverOptions.key = '';
+    serverOptions.cert = '';
+  }
+} catch (error) {
+  console.error('Error reading SSL certificates:', error);
+  serverOptions.key = '';
+  serverOptions.cert = '';
+}
+
+const httpsServer =
+  serverOptions.key && serverOptions.cert
+    ? https.createServer(serverOptions)
+    : https.createServer(); // Fallback to HTTP if SSL files are missing
+
 const wss = new WebSocketServer({ server: httpsServer });
 
-
-httpsServer.listen(7079, () => {
+httpsServer.listen(PORT, () => {
   console.log(
-    'Secure WebSocket server is running on wss://tictactoews.ashishtiwari.net:7079'
+    serverOptions.key && serverOptions.cert
+      ? `Secure WebSocket server is running on wss://tictactoews.ashishtiwari.net:${PORT}`
+      : `WebSocket server is running on ws://tictactoews.ashishtiwari.net:${PORT}`
   );
 });
 
@@ -54,7 +76,7 @@ interface SendData {
 const Manager = new GameManager();
 
 wss.on('connection', (socket, request) => {
-  console.log("request came")
+  console.log('request came');
   try {
     const queryParams = new URLSearchParams(request.url?.split('?')[1]);
     const token = queryParams.get('token');
@@ -98,6 +120,6 @@ wss.on('connection', (socket, request) => {
 
   socket.on('close', () => {
     Manager.handleClose(socket);
-    joinedRooms.delete(socket)
+    joinedRooms.delete(socket);
   });
 });
