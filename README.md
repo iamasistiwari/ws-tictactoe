4f85bbcd0d7a51bf38205f6d413f588149440425
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-02-02 19:19:57 +0530

    authentication improved

---
+++ README.md
@@ -0,0 +1,1 @@
+Websocket for realtime-advance-tictactoe.
--- src/index.ts
+++ src/index.ts
@@ -2,50 +2,9 @@ import { WebSocketServer, WebSocket } from 'ws';
 import GameManager from './GameManager';
 import dotenv from 'dotenv';
 import ValidateUser from './UserValidation';
-import fs from 'fs';
-import https from 'https';
 dotenv.config();
 
-const PORT = process.env.PORT || 7079;
-const SSL_KEY_PATH = process.env.SSL_KEY_PATH || '/etc/certs/privkey.pem';
-const SSL_CERT_PATH = process.env.SSL_CERT_PATH || '/etc/certs/fullchain.pem';
-
-let serverOptions: { key: string | Buffer; cert: string | Buffer } = {
-  key: '',
-  cert: '',
-};
-
-try {
-  if (fs.existsSync(SSL_KEY_PATH) && fs.existsSync(SSL_CERT_PATH)) {
-    serverOptions.key = fs.readFileSync(SSL_KEY_PATH);
-    serverOptions.cert = fs.readFileSync(SSL_CERT_PATH);
-  } else {
-    console.error(
-      'SSL certificate files are missing. Continuing with HTTP server.'
-    );
-    serverOptions.key = '';
-    serverOptions.cert = '';
-  }
-} catch (error) {
-  console.error('Error reading SSL certificates:', error);
-  serverOptions.key = '';
-  serverOptions.cert = '';
-}
-
-const httpsServer =
-  serverOptions.key && serverOptions.cert
-    ? https.createServer(serverOptions)
-    : https.createServer(); // Fallback to HTTP if SSL files are missing
-
-const wss = new WebSocketServer({ server: httpsServer });
-
-httpsServer.listen(PORT, () => {
-  console.log(
-    serverOptions.key && serverOptions.cert
-      ? `Secure WebSocket server is running on wss://tictactoews.ashishtiwari.net:${PORT}`
-      : `WebSocket server is running on ws://tictactoews.ashishtiwari.net:${PORT}`
-  );
-});
+const wss = new WebSocketServer({ port: 7079 });
 
 export const joinedRooms: Map<WebSocket, { joinRooms: string[] }> = new Map();
 
@@ -76,21 +35,21 @@ interface SendData {
 const Manager = new GameManager();
 
 wss.on('connection', (socket, request) => {
-  console.log('request came');
   try {
     const queryParams = new URLSearchParams(request.url?.split('?')[1]);
     const token = queryParams.get('token');
     if (!token) {
       socket.send(JSON.stringify({ type: 'error', message: 'invalid token' }));
+      socket.close()
       return;
     }
     const validatedRequest = ValidateUser(token);
     if (!validatedRequest) {
       socket.send(JSON.stringify({ type: 'error', message: 'invalid token' }));
+      socket.close()
       return;
     }
   } catch (error) {
-    console.log();
   }
 
   joinedRooms.set(socket, { joinRooms: [] });
@@ -120,6 +79,6 @@ wss.on('connection', (socket, request) => {
 
   socket.on('close', () => {
     Manager.handleClose(socket);
-    joinedRooms.delete(socket);
+    joinedRooms.delete(socket)
   });
 });
