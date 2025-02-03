3c6fead2910963ca7a0a5f834c20535668273f3e
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-02-03 22:46:02 +0530

    workflow-added

--- src/UserValidation.ts
+++ src/UserValidation.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 function hashStringWithSecret(input: string): string {
   const secretKey = process.env.SECRET_KEY!;
+  console.log("KEY", secretKey)
   return CryptoJS.HmacSHA256(input, secretKey).toString(CryptoJS.enc.Hex);
 }
 const formatter = new Intl.DateTimeFormat('en-IN', {
@@ -15,8 +16,10 @@ const formatter = new Intl.DateTimeFormat('en-IN', {
 export default function ValidateUser(token: string) {
   const time = formatter.format(new Date());
   const hashedString = hashStringWithSecret(time);
+  console.log("hash",hashedString)
   if (hashedString === token) {
     return true;
   }
   return false;
 }
+ValidateUser("asdasd");
